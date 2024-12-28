b562133b338f37a48bd843fb995ba76de9a29d9c
Eckstein <zac@zaceckstein.com>
2024-12-27 18:38:07 -0800

    Add sender.net header script

--- header.php
+++ header.php
@@ -9,6 +9,20 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
+    <script>
+        (function(s, e, n, d, er) {
+            s['Sender'] = er;
+            s[er] = s[er] || function() {
+                (s[er].q = s[er].q || []).push(arguments)
+            }, s[er].l = 1 * new Date();
+            var a = e.createElement(n),
+                m = e.getElementsByTagName(n)[0];
+            a.async = 1;
+            a.src = d;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://cdn.sender.net/accounts_resources/universal.js', 'sender');
+        sender('fb2eeef11e1128')
+    </script>
 </head>
 
 
